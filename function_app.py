101400111947a9906c748623eeb2c401afb79814
karineos <129395590+karineos@users.noreply.github.com>
2025-01-02 16:47:08 +0200

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -3,29 +3,10 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="fix_script")
-def fix_script(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-
 @app.route(route="openaitesting")
 def openaitest(req: func.HttpRequest) -> func.HttpResponse:
+        logging.info('Python HTTP trigger function processed a request.')
+
     try:
         # Parse the incoming request
         data = req.get_json()
