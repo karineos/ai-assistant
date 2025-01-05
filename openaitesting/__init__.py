7adbc8b0882cecd5e55f21ee912400b63a593f35
karineos <129395590+karineos@users.noreply.github.com>
2025-01-05 11:41:54 +0200

    Update __init__.py

--- openaitesting/__init__.py
+++ openaitesting/__init__.py
@@ -2,7 +2,7 @@ import azure.functions as func
 import logging
 import openai
 import json
-
+import os 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 # Set your OpenAI credentials
